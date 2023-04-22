788796b318494bbe70b764745cc1829f8b3c1531
lost22git <lost22git>
2023-04-23 03:54:14 +0800

    make NATIVE_OPTIONS --enable-monitoring from jfr to all

--- file_walker.java
+++ file_walker.java
@@ -29,7 +29,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring=jfr
+//NATIVE_OPTIONS --no-fallback -H:+ReportExceptionStackTraces --enable-preview --enable-monitoring
 //COMPILE_OPTIONS --enable-preview --release 19
 //RUNTIME_OPTIONS --enable-preview -XX:+UseZGC -XX:NativeMemoryTracking=summary -XX:+HeapDumpOnOutOfMemoryError -XX:StartFlightRecording=filename=jfr/,path-to-gc-roots=true,jdk.ObjectCount#enabled=true
 
